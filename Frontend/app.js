107313fc29dbd92dfafa4fd7117cd71ce6ee629f
Shah-zaib44 <37189038+Shah-zaib44@users.noreply.github.com>
2021-02-23 16:23:53 +0500

    Todo List

--- Frontend/app.js
+++ Frontend/app.js
@@ -1,200 +1,115 @@
-document.addEventListener('DOMContentLoaded',getProductFromLocalStorage);
-document.addEventListener('DOMContentLoaded',showProducts);
+const form = {
+  form: document.querySelector("#task-form"),
+  btnaddTask: document.querySelector("#submit"),
+};
+const taskList = document.querySelector(".collection");
+const clearBtn = document.querySelector(".clear-tasks");
 
-function showProducts(){
-    (async () => {
- 
-        const result = await fetch('http://localhost:5000/api/v1/products', {
-            headers: { "Access-Control-Allow-Origin": "http://localhost:5000/api/v1/products" }
-        });
+const taskInput = document.querySelector("#task");
 
-        const article = await result.json()
+loadEventListeners();
 
-        let i=0;
-  while(i<article.data.recordset.length){
-                      let a=`<div class="shop-item col-4">
-        <span class="shop-item-title">${article.data.recordset[i].product_title}</span>
-        <img class="shop-item-image" src="../public/uploads/${article.data.recordset[i].product_image}">
-        <div class="shop-item-details mb-2">
-            <span class="shop-item-price ">${article.data.recordset[i].product_price}</span>
-            </div>
-            <button class="btn btn-primary shop-item-button mb-5" type="button">ADD TO CART</button>
-                        </div>`;
-    let b=document.querySelector('.shop-items');
-    b.innerHTML+=a;        
-i++;
-       }
+let tasks;
 
-       let addToCartButtons = document.querySelectorAll('.shop-item-button')
-    for (let i = 0; i < addToCartButtons.length; i++) {
-        let button = addToCartButtons[i]
-              button.addEventListener('click', addToCartClicked)
-    }
+tasks = [];
 
-    let removeCartItemButtons = document.querySelectorAll('.btn-danger')
-    for (let i = 0; i < removeCartItemButtons.length; i++) {
-        let button = removeCartItemButtons[i]
-        button.addEventListener('click', removeCartItem)
-    }
+function loadEventListeners() {
+  document.addEventListener("DOMContentLoaded", getTasks);
 
-    let quantityInputs = document.querySelectorAll('.cart-quantity-input')
-    for (let i = 0; i < quantityInputs.length; i++) {
-        let input = quantityInputs[i]
-        input.addEventListener('change', quantityChanged)
-    }
+  form.btnaddTask.addEventListener("click", addTask);
 
-       document.querySelector('.btn-purchase').addEventListener('click', purchaseClicked)
-
-    })();
+  taskList.addEventListener("click", removeTask);
 }
 
-    function addToCartClicked(event) {
-        let button = event.target
-              let shopItem = button.parentElement  
-        let title = shopItem.querySelector('.shop-item-title').innerText
-               let price = shopItem.querySelector('.shop-item-price').innerText
-                 let imageSrc = shopItem.querySelector('.shop-item-image').src
-        addItemToCart(title,price,imageSrc)
-        updateCartTotal()
+function getTasks() {
+  (async () => {
+    const result = await fetch("http://localhost:3000/todolist/task", {
+      headers: {
+        "Access-Control-Allow-Origin": "http://localhost:3000/todolist/task",
+      },
+    });
+    const article = await result.json();
+    console.log(article);
+    article.forEach(function (task) {
+      const li = document.createElement("li");
+
+      li.className = "collection-item";
+
+      li.classList.add(task.id);
+      li.appendChild(document.createTextNode(task.item));
+
+      const link = document.createElement("a");
+
+      link.className = "delete-item secondary-content";
+
+      link.innerHTML = '<i class="fa fa-remove"></i>';
+
+      li.appendChild(link);
+
+      taskList.appendChild(li);
+    });
+  })();
+}
+
+function addTask(e) {
+  if (taskInput.value === "") {
+    alert("Add a task");
+  }
+
+  const li = document.createElement("li");
+
+  li.className = "collection-item";
+
+  li.appendChild(document.createTextNode(taskInput.value));
+
+  const link = document.createElement("a");
+
+  link.className = "delete-item secondary-content";
+
+  link.innerHTML = '<i class="fa fa-remove"></i>';
+
+  li.appendChild(link);
+
+  taskList.appendChild(li);
+
+  tasks.push(taskInput.value);
+
+  fetch(`http://localhost:3000/todolist/createTask/${taskInput.value}`, {
+    method: "post",
+  })
+    .then((response) => response.json())
+    .then(function (response) {
+      console.log(response);
+      if (response.success == true) {
+        alert("Task has been added to database successfully");
+      } else {
+        alert("Unfortunately task has not been added to database");
+      }
+    });
+
+  taskInput.value = "";
+
+  e.preventDefault();
+}
+
+function removeTask(e) {
+  let id = document.getElementsByClassName("collection-item").item(0)
+    .classList[1];
+  fetch(`http://localhost:3000/todolist/task/${id}`, {
+    method: "delete",
+  })
+    .then((response) => response.json())
+    .then(function (response) {
+      if (response.success == true) {
+        alert("item deleted");
+      } else {
+        alert("item cant be deleted");
+      }
+    });
+
+  if (e.target.parentElement.classList.contains("delete-item")) {
+    if (confirm("Are You Sure?")) {
+      e.target.parentElement.parentElement.remove();
     }
-
-    function removeCartItem(event) {
-        let buttonClicked = event.target
-        buttonClicked.parentElement.parentElement.remove()
-     let   cartItemTitle=buttonClicked.parentElement.parentElement.firstElementChild.lastElementChild.innerText
-     removeProductFromLocalStorage(cartItemTitle)
-        updateCartTotal()
-    }
-
-    function quantityChanged(event) {
-        let input = event.target
-        if (isNaN(input.value) || input.value <=0) {
-            input.parentElement.parentElement.remove()
-            let   cartItemTitle=input.parentElement.parentElement.firstElementChild.lastElementChild.innerText
-            removeProductFromLocalStorage(cartItemTitle)
-        }
-        updateCartTotal()
-    }
-
-function purchaseClicked() {
-    alert('Thank you for your purchase')
-    let cartItems = document.querySelector('.cart-items')
-    while (cartItems.hasChildNodes()) {
-        cartItems.removeChild(cartItems.firstChild)
-    }
-    updateCartTotal()
-}
-
-function addItemToCart(title,price,imageSrc) {
-    let cartRow = document.createElement('div')
-    cartRow.classList.add('cart-row')
-    let cartItems = document.querySelector('.cart-items')
-    let cartItemNames = cartItems.querySelectorAll('.cart-item-title')
-    for (let i = 0; i < cartItemNames.length; i++) {
-        if (cartItemNames[i].innerText == title) {
-            alert('This item is already added to the cart')
-            return
-        }
-    }
-    setProductInLocalStorage(title,price,imageSrc)
-    let cartRowContents = `
-        <div class="cart-item cart-column">
-            <img class="cart-item-image" src="${imageSrc}" width="100" height="100">
-            <span class="cart-item-title">${title}</span>
-        </div>
-        <span class="cart-price cart-column">${price}</span>
-        <div class="cart-quantity cart-column">
-            <input class="cart-quantity-input" type="number" value="1">
-            <button class="btn btn-danger" type="button">REMOVE</button>
-        </div>`
-    cartRow.innerHTML = cartRowContents
-    cartItems.append(cartRow)
-
-    cartRow.querySelector('.btn-danger').addEventListener('click', removeCartItem)
-    cartRow.querySelector('.cart-quantity-input').addEventListener('change', quantityChanged)
-
-}
-
-function updateCartTotal() {
-    let cartItemContainer = document.querySelector('.cart-items')
-   
-    let cartRows = cartItemContainer.querySelectorAll('.cart-row')
-    let total = 0
-    for (let i = 0; i < cartRows.length; i++) {
-        let cartRow = cartRows[i]
-        let priceElement = cartRow.querySelector('.cart-price')
-        
-        let quantityElement = cartRow.querySelector('.cart-quantity-input')
-        let price = parseFloat(priceElement.innerText.replace('$', ''))
-        let quantity = quantityElement.value
-        total = total + (price * quantity)
-    }
-    total = Math.round(total * 100) / 100
-    document.querySelector('.cart-total-price').innerText = '$' + total
-}
-
-function setProductInLocalStorage(title,price,imageSrc){
-    let product;
-    if(localStorage.getItem('product')===null)
-    {
-      product=[];
-    }
-    else{ 
-      product=JSON.parse(localStorage.getItem('product'))
-    }
-    product.push({product_title:title,product_price:price,product_image:imageSrc})
-    localStorage.setItem('product',JSON.stringify(product))
-}
-
-function getProductFromLocalStorage(){
-let products;
-if(localStorage.getItem('product')===null)
-{
-  products=[];
-}
-else{ 
-  products=JSON.parse(localStorage.getItem('product'))
-}
-  products=JSON.parse(localStorage.getItem('product'));
-for(let i=0;i<products.length;i++)
-{
-let cartRow = document.createElement('div')
-cartRow.classList.add('cart-row')
-let cartItems = document.querySelector('.cart-items')
-let cartRowContents = `
-    <div class="cart-item cart-column">
-        <img class="cart-item-image" src="${products[i].product_image}" width="100" height="100">
-        <span class="cart-item-title">${products[i].product_title}</span>
-    </div>
-    <span class="cart-price cart-column">${products[i].product_price}</span>
-    <div class="cart-quantity cart-column">
-        <input class="cart-quantity-input" type="number" value="1">
-        <button class="btn btn-danger" type="button">REMOVE</button>
-    </div>`
-cartRow.innerHTML = cartRowContents
-cartItems.append(cartRow)
-
-cartRow.querySelector('.btn-danger').addEventListener('click', removeCartItem)
-cartRow.querySelector('.cart-quantity-input').addEventListener('change', quantityChanged)
-}
-updateCartTotal()
-}
-
-function removeProductFromLocalStorage(cartItemTitle){
-    let products;
-if(localStorage.getItem('product')===null)
-{
-  products=[];
-}
-else{ 
-  products=JSON.parse(localStorage.getItem('product'))
-}
-products.forEach(function(product,index){
-    if(product.product_title==cartItemTitle)
-    {
-   products.splice(index,1)
-      
-    }
-  });
- localStorage.setItem('product',JSON.stringify(products))
+  }
 }
--- middleware/logger.js
+++
@@ -1,7 +0,0 @@
-const logger=(req,res,next)=>{
-    req.hello='Hello friends';
-    console.log(`${req.method} ${req.protocol}://${req.get('host')}${req.originalUrl}`);
-    //console.log('Middleware is running...');
-    next();
-    }
-    module.exports=logger;
